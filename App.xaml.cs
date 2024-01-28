a7f915b77d9b7fa7076a5988991fdb4482c4ec77
Petr Lukáš <157734556+petlukdev@users.noreply.github.com>
2024-01-28 20:47:56 +0100

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using FileExplorer.MVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FileExplorer
+{
+    /// <summary>
+    /// Interakční logika pro App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using FileExplorer.MVVM.Fetchers;
+using System;
+using System.Windows;
+
+
+namespace FileExplorer
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closed(object sender, EventArgs e) => FavoritesFetcher.StoreCollection();
+    }
+}
